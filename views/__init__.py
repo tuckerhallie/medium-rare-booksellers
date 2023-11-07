e50b38791f210e20f09c3747e94ee48526b12243
Olivia Daniels <danielsolivia4199@gmail.com>
2023-11-06 18:50:19 -0600

    comment api

--- views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .comment_requests import get_all_comments
-from .comment_requests import get_single_comment
-from .comment_requests import create_comment
-from .comment_requests import delete_comment
-from .comment_requests import update_comment
-from .comment_requests import get_comments_by_post
